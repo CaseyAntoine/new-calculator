9366a7043c856a3d88ba262042a5a637dc5e2bea
Student <example@example.com>
2019-10-23 11:46:50 -0700

    adjust styling, js functions for calculator.

--- js/scripts.js
+++ js/scripts.js
@@ -22,10 +22,37 @@ $(document).ready(function() {
 
   $("form#add").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#number1").val());
-    var number2 = parseInt($("#number2").val());
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
     var result = add(number1, number2);
-    $(".output").text(result);
+    $(".output1").text(result);
+
+  });
+
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#sub1").val());
+    var number2 = parseInt($("#sub2").val());
+    var result = subtract(number1, number2);
+    $(".output2").text(result);
+
+  });
+
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number1, number2);
+    $(".output3").text(result);
+
+  });
+
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var result = divide(number1, number2);
+    $(".output4").text(result);
 
   });
 
